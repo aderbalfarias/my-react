234cbfcd265981c9c866779a178263e7f94181e0
Aderbal Farias <aderbalfarias@hotmail.com>
2019-01-10 22:37:47 -0200

    Implementing navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { SettingsService } from './services/settings.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     SettingsComponent
   ],
   imports: [
