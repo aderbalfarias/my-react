4ea64cd357f0bfa52eebe253538d84b06963cdbd
Aderbal Farias <aderbalfarias@hotmail.com>
2019-01-30 23:05:40 -0200

    Removing login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { SettingsComponent } from './components/settings/settings.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { LoginComponent } from './components/login/login.component';
 import { SigninComponent } from './components/auth/signin/signin.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
 
@@ -14,7 +13,6 @@ const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'settings', component: SettingsComponent },
     { path: 'notfound', component: NotFoundComponent },
-    { path: 'login', component: LoginComponent },
     { path: 'signin', component: SigninComponent },
     { path: 'signup', component: SignupComponent }
     //{ path: '**', component: DashboardComponent }
